3f9c0d5f895d7583d3c268dc4bd93af389a0f009
IVanzini <ivanadry40@gmail.com>
2023-09-26 14:04:40 +0200

    funzionalità carrello

---
+++ src/app/components/carrello/carrello.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Prodotto } from 'src/app/models/prodotto';
+import { CarrelloService } from 'src/app/services/carrello.service';
+
+@Component({
+  selector: 'app-carrello',
+  templateUrl: './carrello.component.html',
+  styleUrls: ['./carrello.component.css']
+})
+export class CarrelloComponent {
+  prodotti: Prodotto[] = [];
+
+  constructor(public carrelloService: CarrelloService) {
+    
+  }
+}
